84a396117079a2db9c80b7daf0ccda08d7fbd424
Davis Zhang <1145639602@qq.com>
2017-12-27 09:46:16 +0800

    Update Demo.java

--- Date/Calendar/Demo.java
+++ Date/Calendar/Demo.java
@@ -22,3 +22,13 @@ public void set(int feild,int value)
 Calendar c1=Calendar.getInstance();
 c1.add(Calendar.DATE,10)//当前日期加10天
 c1.add(Calendar.DATE,-10)//当前日期减10天
+
+  //Calendar 类对象信息的获取
+  Calendar c1=Calendar.getInstance();
+  int year=c1.get(Calendar.YEAR);//年份
+  int month=c1.get(Calendar.MONTH)+1;//月份
+  int date=c1.get(Calendar.DATE);//日期
+  int hour=c1.get(Calendar.HOUR_OF_DAY);//24小时制
+  int minute=c1.get(Calendar.MINUTE);//分
+  int second=c1.get(Calendar.SECOND);//秒
+  int day=c1.get(Calendar.DAY_OF_WEEK);//1代表周日，2代表周一
