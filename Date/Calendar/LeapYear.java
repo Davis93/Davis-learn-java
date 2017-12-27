fd630ec743d3770cb5bf3373499debe93eb5dce3
Davis Zhang <1145639602@qq.com>
2017-12-27 10:14:01 +0800

    Update LeapYear.java

--- Date/Calendar/LeapYear.java
+++ Date/Calendar/LeapYear.java
@@ -1,3 +1,11 @@
 //4年一闰；百年不闰；4百年再闰；
 import java.util.*;
-public
+public class LeapYear{
+  public static void main(Stringp[] agrs){
+    String []months="Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
+    int year;
+  GregorianCalendar gcalendar= new GregorianCalendar();//公历日历
+  System.out.println("Date:");
+  System.out.println(months[gcalendar.get(Calendar.MONTH)]);
+  
+}
