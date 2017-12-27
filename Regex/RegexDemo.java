c3d49cb3a7609b3e8d17f3e578506cc8980022b5
Davis Zhang <1145639602@qq.com>
2017-12-27 10:38:49 +0800

    Update RegexDemo.java

--- Regex/RegexDemo.java
+++ Regex/RegexDemo.java
@@ -1,1 +1,10 @@
+import java.util.regex.*;
 
+class RegexDemo{
+  public static void main(String[] args){
+  String content ="I am noob"+"from runoob.com";
+  String pattern=".*runoob.com.*";
+  boolean isMatch=Pattern.match(pattern,content);
+  System.out.println("字符串是否包含了runoob.com子字符串？"+isMatch);
+  }
+}
