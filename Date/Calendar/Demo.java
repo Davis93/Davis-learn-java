12154b965c63a2dd3d45987d9f1d9f6da714abb9
Davis Zhang <1145639602@qq.com>
2017-12-27 09:05:35 +0800

    Update Demo.java

--- Date/Calendar/Demo.java
+++ Date/Calendar/Demo.java
@@ -1,2 +1,9 @@
 Calendar c1=Calendar.getInstance();//initialize object
-c1.set(2017,12,27);//set  date
+c1.set(2017,12-1,27);//set  date
+
+
+//set设置
+Calendar c1=Calendar.getInstance();
+public static void set(int year,int month,int year){
+  c1.set(2017,12-1,27)//把Calendar对象c1的年月日分别设这为：2017、12、27
+}
