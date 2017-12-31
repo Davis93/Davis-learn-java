efed00438588de0f5ced91c80d8edca64f2a63f1
Davis Zhang <1145639602@qq.com>
2017-12-31 09:23:21 +0800

    Update RegexMatches.java

--- Regex/RegexMatches.java
+++ Regex/RegexMatches.java
@@ -1,25 +1,23 @@
-import java.regex.Matcher;
-import java.regex.Pattern;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
-public class RegexMatches{
-  public static void main(String[] args){
-    //按指定模式在字符串查找
-    String line="This order was placed for QT3000! OK?";
-    String pattern="(\\D*)(\\d+)(.*)";
-    //创建pattern 对象
-    Pattern r=Pattern.compile(pattern);
-    //创建matcher对象
-    Matcher m=r.matcher(line);
-    if(m.find())
-    {
-      System.out.println("Found value: "+m.group(0));
-      System.out.println("Found value: "+m.group(1));
-      System.out.println("Found value: "+m.group(2));
-      System.out.println("Found value: "+m.group(3));
-    }
-    else{
-      System.out.println("NO MATCH ");
-    }
-  }
-
+public class RegexMatcher {
+	public static void main(String[] args){
+		String line="This order was placed for QT3000! OK?";
+		String pattern="(\\D*)(\\d+)(.*)";//先匹配非[0-9]数字，匹配[0-9数字]，再匹配任意字符
+		 Pattern r= Pattern.compile(pattern);
+		 Matcher m=r.matcher(line);
+		 if(m.find())
+		 {
+			 System.out.println(m.group(0));
+			 System.out.println(m.group(1));
+			 System.out.println(m.group(2));
+			 System.out.println(m.group(3));
+		 }
+		 else
+		 {
+			 System.out.println("No Match!");
+		 }
+	}
 }
+
