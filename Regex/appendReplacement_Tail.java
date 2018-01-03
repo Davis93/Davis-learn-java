c87354ebbdb28463359e77ab924394db8d57f862
Davis Zhang <1145639602@qq.com>
2018-01-03 08:48:15 +0800

    Update appendReplacement_Tail.java

--- Regex/appendReplacement_Tail.java
+++ Regex/appendReplacement_Tail.java
@@ -1,1 +1,10 @@
+// show the difference of between appendReplacement  and appendTail
 
+public class appendReplacement_Tail{
+  public static void main(String[] args){
+  String line="abcabcA@163.com";
+  Pattern p=Pattern("a",Patten.CASE_INSENSITIVE);//case_insensitive 不区分大小写
+    
+  }
+
+}
