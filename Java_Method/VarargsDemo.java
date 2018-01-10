9879f46a9ff46bddea1dafc5120da7e52350ed01
Davis Zhang <1145639602@qq.com>
2018-01-10 16:13:58 +0800

    Update VarargsDemo.java

--- Java_Method/VarargsDemo.java
+++ Java_Method/VarargsDemo.java
@@ -1,1 +1,15 @@
-public class
+public class VarargsDemo{
+    public static void main(String[] args){
+      printMxa(34,3,3,2,56.5);
+       printMxa(new double []{1,2,3});
+    }
+    public static void printMax(double...numbers)
+    {
+        if(numbers.length==0)
+        {
+            System.out.println("No agument passed");
+        }
+    }
+
+
+}
