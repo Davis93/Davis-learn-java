0e7be60cc817b0f4e1a13e8d00a612402136961f
Davis Zhang <1145639602@qq.com>
2018-01-11 21:47:56 +0800

    Create TestVoidMethod1.java

---
+++ Java_Method/TestVoidMethod1.java
@@ -0,0 +1,14 @@
+//通过值传递参数
+public class TestVoidMethod1 {
+	public static void main(String[] args)
+	{
+	   nPrintln("I love u:3344",2);//方法调用
+	}
+	
+	public  static void nPrintln(String message,int n){
+		for(int i=0;i<n;i++)
+		{
+			System.out.println(message);
+		}
+	}
+}
