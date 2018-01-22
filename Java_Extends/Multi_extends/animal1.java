1c0748659642776f85d2dadb1931eeca65c91a84
Davis Zhang <1145639602@qq.com>
2018-01-22 15:54:49 +0800

    Update animal1.java

--- Java_Extends/Multi_extends/animal1.java
+++ Java_Extends/Multi_extends/animal1.java
@@ -1,1 +1,23 @@
+//父类，用于extends和implements继承
+public class animal1 {
+	private String name;
+	private int id;
+	public animal1(String myName,int myId)//构造器
+	{
+		name=myName;
+		id=myId;
+	}
+	public void eat()
+	{
+		System.out.println("Eating!");
+	}
 
+	public void sleep()
+	{
+		System.out.println("Sleeping!");
+	}
+	public void introduction()
+	{
+		System.out.println("Hello,everyone,my name is: "+name+",myid is: "+id);
+	}
+}
