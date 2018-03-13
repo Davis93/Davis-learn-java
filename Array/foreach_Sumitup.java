52e5ccaa9775f0bf6054b256220b5aa1c39a4907
Davis Zhang <1145639602@qq.com>
2018-03-13 20:01:32 +0800

    Create foreach_Sumitup.java

---
+++ Array/foreach_Sumitup.java
@@ -0,0 +1,16 @@
+//foreach demo and sum it up of array;
+public class Sumitup {
+	public static void main(String[] args){
+		double[] myList={5.6,4.5,3.3,13.2,4.0,34.33,34.0,45.45,99.993,11123};
+		for(double x:myList)
+		{
+			System.out.println(x);//foreach 遍历方法
+		}
+		double total=0;
+		for(int i=0;i<myList.length;i++)
+		{
+			total+=myList[i];
+		}
+		System.out.println(total);
+	}
+}
