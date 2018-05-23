f3d092a1b7909781483650edfa2165e060895ceb
Davis Zhang <1145639602@qq.com>
2018-05-23 10:24:44 +0800

    Update and rename FileStringOut.java to FileStringOut1.java

---
+++ Java_IO/FileStringOut1.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class FileStringOut1 {
+	public static void main(String[] args) throws IOException
+	{
+		File f1=new File("E:/Test/zf1.txt");
+		PrintWriter pw=new PrintWriter(f1);
+		pw.println("a");
+		pw.println("b");
+		pw.println("c");
+		pw.close();
+	}
+
+}
